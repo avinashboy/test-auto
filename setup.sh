584393de48f0d58b550ab520d8361c2a7b3c2dc5
avinashboy <52669990+avinashboy@users.noreply.github.com>
2022-11-12 19:15:39 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+#set the time zone
+timedatectl set-timezone  Africa/Accra
+
+#set the RTC  to false
+timedatectl set-local-rtc false
+
+#set the system host name
+#sudo hostnamectl set-hostname kiosk
+
+#update the package
+#sudo apt-get update
+
+#upgrade the package
+#sudo apt-get upgrade
+
+#reboot the system
+#sudo reboot
